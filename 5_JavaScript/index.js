b2b3cb97e1caafb9a5fb4ce865b54d67a5f67118
bovZoran <12100704@student.pxl.be>
2022-05-28 15:11:18 +0200

    commited

--- 5_JavaScript/index.js
+++ 5_JavaScript/index.js
@@ -4,3 +4,15 @@ console.log('Test this works');
 
 let name = 'Zoran';
 console.log(name);
+
+let x;
+
+
+document.getElementById("rollButton").onclick = function(){
+
+    x = Math.floor(Math.random() * 21) + 1;
+
+
+    document.getElementById("xlabel").innerHTML = x;
+
+}
